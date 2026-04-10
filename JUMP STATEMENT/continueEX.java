eb977086f7253e8c3dd621951d86dba320882978
pallaviugale <pallaviugale315@gmail.com>
2026-04-10 09:43:05 +0530

    iteration nd jump statement

---
+++ JUMP STATEMENT/continueEX.java
@@ -0,0 +1,14 @@
+
+
+public class continueEX {
+    public static void main(String[] args) {
+        for (int num = 1; num<=10;num++){
+         if (num==3){
+            continue;
+        }
+        System.out.println(num);
+        }
+        
+    }
+    
+}
--- SELECTION STATEMENT/switch_example.java
+++ SELECTION STATEMENT/switch_example.java
@@ -1,6 +1,6 @@
 public class switch_example {
     public static void main(String[] args) {
-        int day = 2;
+        int day = 5;
         switch (day) {
             case 1 :System.out.println("Monday");
                 
